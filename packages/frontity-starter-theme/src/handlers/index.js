1d7ea78bc3b28c89db5a469794d7ca5181b26a9a
Luis Herranz <luisherranz@gmail.com>
2020-03-20 14:03:59 +0100

    Add handler for /about that also fetches projects

--- packages/frontity-starter-theme/src/handlers/index.js
+++ packages/frontity-starter-theme/src/handlers/index.js
@@ -1,22 +1,45 @@
 export const projectsHandler = {
-  name: "projects",
+  name: "about",
   priority: 10,
-  pattern: "/projects/",
-  func: async ({ route, params, state, libraries }) => {
-    //1-get projects
-    const response = await libraries.source.api.get({
-      endpoint: "projects"
-      // params: { slug: params.slug }
+  pattern: "/about",
+  func: async ({ route, state, libraries }) => {
+    // Fetch the about page.
+    const aboutResponse = await libraries.source.api.get({
+      endpoint: "pages",
+      params: { slug: "about", _embed: true }
     });
-    //2- add project to state
-    const [project] = await libraries.source.populate({ response, state });
 
-    //3- add route to data
+    // Populate the state with the response.
+    const [aboutData] = await libraries.source.populate({
+      response: aboutResponse,
+      state
+    });
+
+    // Extract the project IDs.
+    const aboutPage = state.source.page[aboutData.id];
+    const projectIds = aboutPage.acf.flexible_layouts
+      .find(layout => layout.acf_fc_layout === "projects_block")
+      .projects.map(project => project.ID);
+
+    // Fetch the projects with those ids.
+    const projectsResponse = await libraries.source.api.get({
+      endpoint: "projects",
+      params: { includes: projectIds.join(",") }
+    });
+
+    // Populate the state with those projects.
+    await libraries.source.populate({
+      response: projectsResponse,
+      state
+    });
+
+    // Populate data with info about /about.
     Object.assign(state.source.data[route], {
-      id: project.id,
-      type: project.type,
+      id: aboutData.id,
+      type: aboutData.type,
+      isAboutPage: true,
       isPostType: true,
-      isProject: true
+      projectIds
     });
   }
 };
